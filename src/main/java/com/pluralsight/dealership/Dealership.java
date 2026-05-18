661a65b99db6cf168fcf5a8aa86a558b159e434e
Joy-ConScion <chris.torres2001@outlook.com>
2026-05-17 23:40:43 -0500

    Add required methods

--- src/main/java/com/pluralsight/dealership/Dealership.java
+++ src/main/java/com/pluralsight/dealership/Dealership.java
@@ -3,58 +3,92 @@ package com.pluralsight.dealership;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class Dealership {
-        private String name;
-        private String address;
-        private String phone;
-        private ArrayList<Vehicle> inventory;
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
 
-        public Dealership(String name, String address, String phone) {
-            this.name = name;
-            this.address = address;
-            this.phone = phone;
-            this.inventory = new ArrayList<>();
-        }
-
-
-        public String getName(){
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        public String getPhone() {
-            return phone;
-        }
-
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
-
-        public List<Vehicle> getAllVehicles(){
-            return inventory; /*Return later*/
-        }
-
-        public void addVehicle(String line){
-            String[] vehicleFields = line.split("\\|");
-            Vehicle vehicle = new Vehicle(Integer.parseInt(vehicleFields[0]), Integer.parseInt(vehicleFields[1]), vehicleFields[2], vehicleFields[3], vehicleFields[4], vehicleFields[5], Integer.parseInt(vehicleFields[6]), Double.parseDouble(vehicleFields[7]));
-            inventory.add(vehicle);
-        }
-
-        public Dealership() {
-            name = "";
-            address = "";
-            phone = "";
-            inventory = new ArrayList<>();
-        }
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
     }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return inventory; /*Return later*/
+    }
+
+    public void addVehicle(String line) {
+        String[] vehicleFields = line.split("\\|");
+        Vehicle vehicle = new Vehicle(Integer.parseInt(vehicleFields[0]), Integer.parseInt(vehicleFields[1]), vehicleFields[2], vehicleFields[3], vehicleFields[4], vehicleFields[5], Integer.parseInt(vehicleFields[6]), Double.parseDouble(vehicleFields[7]));
+        inventory.add(vehicle);
+    }
+
+    public Dealership() {
+        name = "";
+        address = "";
+        phone = "";
+        inventory = new ArrayList<>();
+    }
+
+    public List<Vehicle> getPricedVehicle(double min, double max) {
+        List<Vehicle> vehicleMatch = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max) {
+                vehicleMatch.add(vehicle);
+            }
+        }
+        return vehicleMatch;
+    }
+
+    public List<Vehicle> getVehicleMakeModel(String maker, String model) {
+        List<Vehicle> vehicleMatch = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getMake().equalsIgnoreCase(maker) && vehicle.getModel().equalsIgnoreCase(model)) {
+                vehicleMatch.add(vehicle);
+            }
+        }
+        return vehicleMatch;
+    }
+
+    public List<Vehicle> getVehicleAge(double min, double max) {
+        List<Vehicle> vehicleMatch = new ArrayList<>();
+
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max) {
+                vehicleMatch.add(vehicle);
+            }
+        }
+        return vehicleMatch;
+    }
+
+}
--- src/main/java/com/pluralsight/dealership/UserInterface.java
+++ src/main/java/com/pluralsight/dealership/UserInterface.java
@@ -21,13 +21,12 @@ public class UserInterface {
         while (runApp) {
             System.out.println("""
                     ==================================================
-                     ___            ____          __   ____  ____         ___      ___   ____  _        ___  ____     _____  __ __  ____  ____ 
-                    |   \\          |    \\        /  ] /    ||    \\    |   \\   /  _] /    || |      /  _]|    \\  / ___/ |  |  ||    ||    \
-                    |    \\         |  o  )      /  / |  o  ||  D  )     |    \\ /  [_ |  o  || |     /  [_ |  D  ) (  \\_  |  |  | |  | |  o  )
-                    |  D  |        |     |     /  /  |     ||    /       |  D  ||    _]|     || |___ |    _]|    /  \\__   ||  _  | |  | |   _/
-                    |     |        |  O  |    /   \\_ |  _  ||    \\     |     ||   [_ |  _  ||     ||   [_ |    \\  /  \\ ||  |  | |  | |  |  
-                    |     |        |     |    \\     ||  |  ||  .  \\    |     ||     ||  |  ||     ||     ||  .  \\ \\    ||  |  | |  | |  |  
-                    |_____|        |_____|     \\____||__|__||__|\\_|    |_____||_____||__|__||_____||_____||__|\\_|  \\___||__|__||____||__|  
+                     ___        ____           __   ____  ____        ___      ___   ____  _        ___  ____     _____  __ __  ____  ____ 
+                    |   \\     |    \\        / ] /     ||    \\     |   \\   /  _] /    || |      /  _]|    \\  / ___/ |  |  ||    ||    \
+                    |    \\    |  o  )      /  /  |  o  ||  D  )     |    \\ /  [_ |  o  || |     /  [_ |  D  ) (  \\_  |  |  | |  | |  o  )
+                    |  D  |    |     |     /  /   |     ||    /      |  D  ||    _]|     || |___ |    _]|    /  \\__   ||  _  | |  | |   _/
+                    |     |    |  O  |    /   \\_ |  _  ||    \\     |     ||   [_ |  _  ||     ||   [_ |    \\   _ \\ ||  |  | |  | |  |  
+                    |_____|    |_____|     \\____||__|__||__|\\_|    |_____||_____||__|__||_____||_____||__|\\_|  \\___||__|__||____||__|  
                     
                     
                     
@@ -77,13 +76,50 @@ private void displayVehicles(List<Vehicle>filteredVehicles){
 
 public void getByPriceRequest(){
 
+        boolean runPrice = false;
+
+        while (!runPrice){
+            System.out.println("Minimum price point? ");
+                double minPrice = keyboard.nextDouble();
+            System.out.println("Maximum price point? ");
+                double maxPrice = keyboard.nextDouble();
+            keyboard.nextLine();
+
+            displayVehicles(dealership.getPricedVehicle(minPrice, maxPrice));
+            runPrice = true;
+        }
+
 }
 
 public void getByMakeModelRequest(){
 
+    System.out.println("Maker of vehicle? ");
+        String maker = keyboard.nextLine();
+    System.out.println("Model of vehicle? ");
+        String model = keyboard.nextLine();
+
+    displayVehicles(dealership.getVehicleMakeModel(maker, model));
+
 }
 
 public void getByYearRequest(){
+
+boolean runAge = false;
+
+while (!runAge) {
+    System.out.println("Youngest year for vehicle? ");
+    int min = keyboard.nextInt();
+    System.out.println("Oldest year for vehicle? ");
+    int max = keyboard.nextInt();
+    keyboard.nextLine();
+
+    displayVehicles(dealership.getVehicleAge(min, max));
+    runAge = true;
+
+}
+
+
+
 
 }
 
